888487fb4c6962c845fe1ea867e321f565aabb8e
Henrique <henriquesouza5114@gmail.com>
2025-07-02 09:38:33 -0300

    Add files via upload

---
+++ Calculadora_Simples.py
@@ -0,0 +1,60 @@
+import os
+os.system('cls')
+
+def somar(a,b): #Função que irá somar
+    return a+b
+
+def subtrair(a,b): #Função que irá subtrair
+    return a-b
+
+def multiplicar(a,b): #Função que irá multiplicar
+    return a*b
+
+def dividir(a,b): #Função que irá dividir
+    if b==0:
+        return "Erro! Divisão por zero!" #Irá exibir uma mensagem de erro caso o seja feita a tentativa de divisão e algum número por zero
+    return a/b
+
+def calculadora_simples():
+    print('='*33)
+    print("=======Calculadora Simples=======")
+    print('='*33)
+    
+    
+    #Entrada dos Valores e Operações
+    try: #Try e Except = Tratamento de erros para evitar que o programa quebre.
+        num1=float(input("Digite um número: "))
+        print('-'*33)
+        operacao=str(input("Digite uma operação (+,-,*,/): "))
+        print('-'*33)
+        num2=float(input("Digite um número: "))
+        print('-'*33)
+        
+    except ValueError:
+        print("Entrada Inválida!Por favor digite números válidos!")
+        return
+    
+    if operacao == '+':
+        resultado = somar (num1,num2)
+        
+    elif operacao == '-':
+        resultado = subtrair (num1,num2)
+        
+    elif operacao == "*":
+        resultado = multiplicar (num1,num2)
+        
+    elif operacao == '/':
+        resultado = dividir (num1,num2)
+        
+    else:
+        resultado = 'Operação Inválida!'
+        
+    print("Resultado: ", resultado)
+    print('='*33)
+
+#Irá executar a calculadora    
+calculadora_simples()    
+    
+    
+
+
