8f0842c430a5fe97456d9f30b59b04b61140e28a
Henrique <henriquesouza5114@gmail.com>
2025-07-02 08:43:41 -0300

    Add files via upload

---
+++ Usuário_e_Senha_Simples.py
@@ -0,0 +1,22 @@
+import os
+os.system('cls')
+
+
+user_correct = 'admin' #Define o nome do usuário.
+password_correct = '12345' #Define a senha do usuário.
+
+#Área onde o usuário irá digitar suas informações.
+user = input("Digite seu usuário: ")
+password = input("Digite sua senha: ")
+
+#Irá verificar se Usuário e Senha digitados pelo usúario são os mesmos cadastrados.
+if user == user_correct and password == password_correct: 
+    print('Login bem sucedido!')
+
+#Se o Usuário ou Senha estiver incorretos irá voltar está mensagem ao Usuário.  
+else:
+    print('Usuário ou Senha Incorretos!')
+        
+    
+    
+
